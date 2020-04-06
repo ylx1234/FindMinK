a66d3e221ddb77fc1638991b8eac8701039ea82c
ylx1234 <46913518+ylx1234@users.noreply.github.com>
2020-04-06 23:30:18 +0800

    Add files via upload

---
+++ 作业6/FindMinK/FindMinK/源.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int FindMinK(int data[], int l, int r, int k)
+{
+	int m = data[l + k - 1], t = 0;
+	int i, j;
+	data[l + k - 1] = data[r], data[r] = m;
+	for (i = l - 1, j = l; j != r; ++j)
+		if (data[j] < m)
+			t = data[++i], data[i] = data[j], data[j] = t;
+	t = data[++i], data[i] = data[j], data[j] = t;
+	if (k == i - l + 1)
+		return m;
+	else if (k > i - l + 1) 
+		return FindMinK(data, i + 1, r, k - i + l - 1);
+	else   
+		return FindMinK(data, l, i - 1, k);
+}
+int main()
+{
+	int data[] = { 8,6,6,4,4,3,2,1 };
+	int k = 3;
+	printf("%d\n", FindMinK(data, 0, 7, k));
+	return 0;
+}
